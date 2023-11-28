ec6cb7d8cccb931a14b48ce3b1c729ffae3d8449
Pavlo Honcharevskyi <fix.pgoncharevskiy@gmail.com>
2023-11-28 02:27:03 +0200

    done

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Bookstore.src;
+using BookStore.src;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BookStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("E:\\PROG3\\appsettings.json");
+
+            // создаем конфигурацию
+            var config = builder.Build();
+
+            // получаем строку подключения
+            String connectionString = config.GetConnectionString("MyConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            var options = optionsBuilder.UseSqlServer(connectionString).Options;
+
+            using (var context = new MyContext(options))
+            {
+
+                var clientToAdd = new Client() { FirstName = "John", LastName = "Morgan", DateOfBirth = new DateTime(), PhoneNumber = "+380662389433" };
+
+                context.Clients.Add(clientToAdd);
+                context.SaveChanges();
+
+
+                var clientToChange = context.Clients.Where(c => c.FirstName == "John").FirstOrDefault();
+                if (clientToChange != null)
+                {
+                    clientToChange.PhoneNumber = "+380662389777";
+                }
+                context.SaveChanges();
+
+
+                var clientToDelete = context.Clients.Where(c => c.FirstName == "Julia").FirstOrDefault();
+                context.Clients.Remove(clientToDelete);
+                context.SaveChanges();
+
+                var clientss = context.Clients.ToList();
+
+                foreach (var client in clientss)
+                {
+                    Console.WriteLine(client.FirstName + " " + client.PhoneNumber + "\n");
+                }
+            }
+        }
+    }
+}
---
+++ src/Book.cs
@@ -0,0 +1,52 @@
+﻿using Bookstore.src;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookStore
+{
+    public enum Genres
+    {
+        Drama, 
+        Fantasy, 
+        FairyTale, 
+        TravelBooks,
+        Autobiography,
+        History,
+        Thriller,
+        Mystery,
+        Romance,
+        Horror,
+        Business
+    }
+
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public Genres Genre { get; set; }
+
+        [MaxLength(100)]
+        public String Name { get; set; }
+
+        public Author Author { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+
+        public List<Edition> Editions = new List<Edition>();
+
+
+        public Book()
+        { }
+
+        public Book(Author author, Genres genre, String name)
+        {
+            Author = author;
+            Genre = genre;
+            Name = name;
+        }
+    }
+}
